4c3da2f6b9208d08b96d200baef6f748fc5519ff
木城崇 <49614111+mc18wc@users.noreply.github.com>
2021-04-20 10:13:30 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,32 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite2.setVelocity(20, -50)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    mySprite2.vy = 0 - mySprite2.vy
+    mySprite2.startEffect(effects.fountain, 100)
+    scene.cameraShake(2, 100)
+})
+let mySprite2: Sprite = null
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . 8 1 1 1 1 1 1 1 1 1 1 1 1 8 . 
+    8 9 9 9 9 9 9 9 9 9 9 9 9 9 9 8 
+    8 9 8 9 8 9 8 9 8 9 8 9 8 9 8 8 
+    . 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+mySprite2 = sprites.create(img`
+    1 d 
+    d d 
+    `, SpriteKind.Projectile)
+mySprite.setPosition(76, 105)
+mySprite2.setPosition(mySprite.x, 102)
+mySprite.setStayInScreen(true)
+mySprite2.setBounceOnWall(true)
+controller.moveSprite(mySprite, 100, 0)
+let 开始 = 0
+forever(function () {
+	
+})
