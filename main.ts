345a91c28fd3beec6d855e197c020d71f60f2e47
木城崇 <49614111+mc18wc@users.noreply.github.com>
2021-04-20 21:01:29 +0800

    更新 main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
+namespace SpriteKind {
+    export const ball = SpriteKind.create()
+}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite2.setVelocity(20, -50)
 })
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+sprites.onOverlap(SpriteKind.Player, SpriteKind.ball, function (sprite, otherSprite) {
     mySprite2.vy = 0 - mySprite2.vy
     mySprite2.startEffect(effects.fountain, 100)
     scene.cameraShake(2, 100)
@@ -20,9 +23,9 @@ let mySprite = sprites.create(img`
 mySprite2 = sprites.create(img`
     1 d 
     d d 
-    `, SpriteKind.Projectile)
-mySprite.setPosition(76, 105)
-mySprite2.setPosition(mySprite.x, 102)
+    `, SpriteKind.ball)
+mySprite.setPosition(76, 110)
+mySprite2.setPosition(mySprite.x, 107)
 mySprite.setStayInScreen(true)
 mySprite2.setBounceOnWall(true)
 controller.moveSprite(mySprite, 100, 0)
